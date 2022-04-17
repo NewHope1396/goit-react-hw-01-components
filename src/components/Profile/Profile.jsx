8a9b3fbb40b18ee21c21bd7179993ec4198c546e
NewHope <rimondtt@gmail.com>
2022-04-18 00:14:45 +0300

    task01-proptypes-added

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import { Container, Image, Name, Description, DescriptionText, Stats, StatsItem, Numbers, Label } from "./Profile.styled"
 
 export const Profile = ({username, tag, location, avatar, stats}) => (
@@ -28,3 +29,17 @@ export const Profile = ({username, tag, location, avatar, stats}) => (
         </Stats>
     </Container>
 )
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape(
+    {
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired
+    }
+  )
+}
